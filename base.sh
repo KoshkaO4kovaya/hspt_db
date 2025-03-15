1be631de3fa1a2f494a3060da8ad63cebd3b82e1
stud <stud@it224-13.almetpt.local>
2025-03-15 08:39:31 +0300

    Automatic commit

---
+++ base.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Получаем имя удаленного репозитория от пользователя
+read -p "Введите имя удаленного репозитория: " remote_name
+
+# Получаем имя ветки от пользователя
+read -p "Введите имя ветки: " branch_name
+
+# Сообщение коммита
+commit_message="Automatic commit"
+
+# Добавляем все изменения
+git add .
+
+git commit -m "$commit_message"
+
+git push "$remote_name" "$branch_name"
